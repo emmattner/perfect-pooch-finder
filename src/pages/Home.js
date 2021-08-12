599ea0d90c7d6c84c137f90fae2f9973485054d8
Emma Mattner <emattner1997@gmail.com>
2021-08-12 16:00:24 +0930

    added lazy hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import LazyHero from 'react-lazy-hero';
+
+
+
+
 
 const Home = () => {
   return (
     <div className="container">
-      <h1>Home</h1>
+      
+      <div>
+            <LazyHero imageSrc="https://images.dog.ceo/breeds/pointer-german/n02100236_5647.jpg" >
+                <h1 className="h1LazyHero">Time to Find Your Perfect Pooch!</h1>
+            </LazyHero>
+      </div>
+      
+
     </div>
   );
 };
