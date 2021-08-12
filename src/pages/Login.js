2a1d63a63b4d12ef4edf88b30518aefe4347e30d
Emma Mattner <emattner1997@gmail.com>
2021-08-12 16:00:09 +0930

    imported form for semantic ui

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,8 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../api/mutations';
 import Auth from '../state/auth';
+import { Form } from 'semantic-ui-react';
+
 
 function Login() {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -35,14 +37,12 @@ function Login() {
 
       <h2>Login</h2>
       <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input placeholder="youremail@test.com" name="email" type="email" id="email" onChange={handleChange} />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input placeholder="******" name="password" type="password" id="pwd" onChange={handleChange} />
-        </div>
+        <Form.Group widths='equal'>
+          <Form.Input fluid label='Email Address' placeholder='youremail@address.com' id='email' onChange={handleChange} />
+          <br></br>
+          <Form.Input fluid label='Password' placeholder='*****' id='pwd' onChange={handleChange} />
+        </Form.Group>  
+        <br></br>
         {error ? (
           <div>
             <p className="error-text">The provided credentials are incorrect</p>
@@ -54,6 +54,34 @@ function Login() {
       </form>
     </div>
   );
+
 }
 
 export default Login;
+
+{/* <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email address:</label>
+          <input placeholder="youremail@test.com" name="email" type="email" id="email" onChange={handleChange} />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input placeholder="******" name="password" type="password" id="pwd" onChange={handleChange} />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="age">Age:</label>
+          <input placeholder="Age" name="age" type="age" id="age" onChange={handleChange} />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="yardSize">Size of yard:</label>
+          <input placeholder="" name="password" type="password" id="pwd" onChange={handleChange} />
+        </div>
+        {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null}
+        <div className="flex-row flex-end">
+          <button type="submit">Submit</button>
+        </div> */}
+
+      
